4f63da6d8d0248a53c25a6d96ab8c00e9628f1b8
srujaal <rahulsharmamuthilla@ymail.com>
2014-11-04 13:24:24 +0530

    Update MainActivity.java

--- src/com/jash/bunkin/MainActivity.java
+++ src/com/jash/bunkin/MainActivity.java
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import android.app.Activity;
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.FragmentTransaction;
@@ -23,7 +24,10 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Toast;
+import android.widget.ImageView;
 
 import com.jash.bunkin.Adapters.SectionsPagerAdapter;
 import com.jash.bunkin.actions.ComposeActivity;
@@ -367,3 +371,54 @@ public class MainActivity extends FragmentActivity implements
 	}
 
 }
+        public class MainActivity extends Activity {
+	private static final int[] ITEM_DRAWABLES = { R.drawable.composer_camera, R.drawable.composer_music,
+			R.drawable.composer_place, R.drawable.composer_sleep, R.drawable.composer_thought, R.drawable.composer_with };
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		ArcMenu arcMenu = (ArcMenu) findViewById(R.id.arc_menu);
+        ArcMenu arcMenu2 = (ArcMenu) findViewById(R.id.arc_menu_2);
+
+        initArcMenu(arcMenu, ITEM_DRAWABLES);
+        initArcMenu(arcMenu2, ITEM_DRAWABLES);
+
+		RayMenu rayMenu = (RayMenu) findViewById(R.id.ray_menu);
+        final int itemCount = ITEM_DRAWABLES.length;
+		for (int i = 0; i < itemCount; i++) {
+			ImageView item = new ImageView(this);
+			item.setImageResource(ITEM_DRAWABLES[i]);
+
+			final int position = i;
+			rayMenu.addItem(item, new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+				}
+			});// Add a menu item
+		}
+	}
+
+    private void initArcMenu(ArcMenu menu, int[] itemDrawables) {
+        final int itemCount = itemDrawables.length;
+        for (int i = 0; i < itemCount; i++) {
+            ImageView item = new ImageView(this);
+            item.setImageResource(itemDrawables[i]);
+
+            final int position = i;
+            menu.addItem(item, new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
+
